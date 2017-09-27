a5671d78295e6bec1d5e72c19cef003719e262dc
AbramOmovich <AbramOmovich@gmail.com>
2017-09-27 09:18:05 +0300

    started analyzer

--- App/Application.php
+++ App/Application.php
@@ -2,38 +2,94 @@
 
 namespace UPSS\App;
 
+use UPSS\App\Core\Analyzer;
 use UPSS\App\Preprocessors\CompatibilityValidator;
 use UPSS\App\Preprocessors\ObjectValidator;
 use UPSS\App\Preprocessors\PreferenceValidator;
 
 class Application
 {
+    private const STATE_NOTHING = 0;
+    private const STATE_OBJECTS = 1;
+    private const STATE_PREFERENCES = 2;
+
+    private $state;
+
     public function handleRequest(Request $request)
     {
+        $this->detectState($request);
+
         try {
             $this->sendToValidator($request);
         } catch (\Exception $e) {
             return new Response($e);
         }
 
-        return new Response();
+        $responseData = $this->prepareResponse($request);
+        return new Response($responseData);
     }
 
-    public function sendToValidator($request)
+    private function detectState(Request $request)
     {
-        if (isset($request['objects'])) {
-            $validator = new ObjectValidator($request['objects']);
-            $validObjects = $validator->validate();
+        $this->state = self::STATE_NOTHING;
+        if (isset($request['objects'])) $this->state = $this->state | self::STATE_OBJECTS;
+        if (isset($request['preferences'])) $this->state = $this->state | self::STATE_PREFERENCES;
+    }
+
+    private function sendToValidator(Request $request)
+    {
+        switch ($this->state){
+            case self::STATE_OBJECTS :
+                $validator = new ObjectValidator($request['objects']);
+                $validObjects = $validator->validate();
+                break;
+
+            case self::STATE_PREFERENCES and self::STATE_OBJECTS :
+                $validator = new ObjectValidator($request['objects']);
+                $validObjects = $validator->validate();
+
+                if(!empty($request['preferences'])) {
+                    $validator = new PreferenceValidator($request['preferences']);
+                    $validPrefs = $validator->validate();
+
+                    $validator = new CompatibilityValidator($validObjects, $validPrefs);
+                    $validator->validate();
+                }
+                break;
+        }
+    }
+
+    private function prepareResponse(Request $request)
+    {
+        switch ($this->state){
+            case self::STATE_OBJECTS :
+                $preferences = $this->initPreferences(array_keys($request['objects'][0]));
+                return $preferences;
+
+            case self::STATE_PREFERENCES and self::STATE_OBJECTS :
+                $this->sendToAnalyzer($request);
+                break;
+        }
+        return null;
+    }
+
+    private function initPreferences($prefs)
+    {
+        foreach ($prefs as &$pref){
+            $pref = [
+                $pref => [
+                    'direction' => 1, // 1 stands for max, 0 for min
+                    'weight' => 0.5   // Less important param has less weight
+                ]
+            ];
         }
 
-        if (isset($request['preferences']) && !empty($request['preferences'])) {
-            $validator = new PreferenceValidator($request['preferences']);
-            $validPrefs = $validator->validate();
-        }
+        return $prefs;
+    }
 
-        if (isset($validObjects) && isset($validPrefs)){
-            $validator = new CompatibilityValidator($validObjects, $validPrefs);
-            $validator->validate();
-        }
+    private function sendToAnalyzer($data)
+    {
+        $analyzer = new Analyzer($data);
+        $analyzer->analyze();
     }
 }
--- App/Core/Analyzer.php
+++ App/Core/Analyzer.php
@@ -2,7 +2,29 @@
 
 namespace UPSS\App\Core;
 
-class Analizer
+class Analyzer
 {
+    protected $objects;
+    protected $preferences;
+
+    protected $objectProps;
+    protected $filteredObjects;
+
+    public function __construct($data)
+    {
+        $this->objects = $data['objects'];
+        $this->objectProps = array_keys($this->objects[0]);
+        $this->preferences = $data['preferences'];
+    }
+
+    public function analyze()
+    {
+
+    }
+
+    protected function filterObjects()
+    {
+
+    }
 
 }
